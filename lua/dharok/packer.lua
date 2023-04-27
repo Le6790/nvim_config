b110e06de1c64c7545a03c85cd8ee938df68eeba
Kent Le <le6790@gmail.com>
2023-04-26 23:07:53 -0600

    updated colorscheme from rose-pine to kanagawa

--- lua/dharok/packer.lua
+++ lua/dharok/packer.lua
@@ -14,14 +14,14 @@ return require('packer').startup(function(use)
     --	  -- or                            , branch = '0.1.x',
     --	  requires = { {'nvim-lua/plenary.nvim'} }
     --  }
-
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
+    use "rebelot/kanagawa.nvim"
+    -- use({
+    --     'rose-pine/neovim',
+    --     as = 'rose-pine',
+    --     config = function()
+    --         vim.cmd('colorscheme rose-pine')
+    --     end
+    -- })
 
     use('preservim/nerdtree')
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
